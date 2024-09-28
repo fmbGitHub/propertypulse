440ffc53e30d58c389c5d0edf335198808cdbfea
Felix Melendez <ferumeba@hotmail.com>
2024-09-28 14:39:39 -0600

    getSessionUser with no trycatch

--- utils/getSessionUser.js
+++ utils/getSessionUser.js
@@ -2,19 +2,32 @@ import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/utils/authOptions';
 
 export const getSessionUser = async () => {
-  try {
-    const session = await getServerSession(authOptions);
+  // try {
+  //   const session = await getServerSession(authOptions);
 
-    if (!session || !session.user) {
-      return null;
-    }
+  //   if (!session || !session.user) {
+  //     return null;
+  //   }
 
-    return {
-      user: session.user,
-      userId: session.user.id,
-    };
-  } catch (error) {
-    console.error(error);
+  //   return {
+  //     user: session.user,
+  //     userId: session.user.id,
+  //   };
+  // } catch (error) {
+  //   console.error(error);
+  //   return null;
+  // }
+  // NOTE: Here we have removed the try catch block so that at build time NextJS
+  // can catch the error and know not to try and statically generate pages that
+  // require a auth session.
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
     return null;
   }
+
+  return {
+    user: session.user,
+    userId: session.user.id,
+  };
 };
