471730c8e92363842bac6b0660d894034b3f95a8
Felix Melendez <ferumeba@hotmail.com>
2024-09-28 14:48:01 -0600

    database.js with no trycatch and error page created

--- config/database.js
+++ config/database.js
@@ -3,22 +3,36 @@ import mongoose from 'mongoose';
 let connected = false;
 
 const connectDB = async () => {
+  // mongoose.set('strictQuery', true);
+
+  // // If the database is already connected, don't connect again
+  // if (connected) {
+  //   console.log('MongoDB is already connected');
+  //   return;
+  // }
+
+  // // Connect to MongoDB
+  // try {
+  //   await mongoose.connect(process.env.MONGODB_URI);
+  //   connected = true;
+  //   console.log('MongoDB connected...');
+  // } catch (error) {
+  //   console.log(error);
+  // }
+  // NOTE: here we don't need to catch a error and log to the console as any
+  // thrown error will be caught by our error page
   mongoose.set('strictQuery', true);
 
   // If the database is already connected, don't connect again
   if (connected) {
-    console.log('MongoDB is already connected');
+    console.log('MongoDB is already connected...');
     return;
   }
 
   // Connect to MongoDB
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    connected = true;
-    console.log('MongoDB connected...');
-  } catch (error) {
-    console.log(error);
-  }
+  await mongoose.connect(process.env.MONGODB_URI);
+  connected = true;
+  console.log('MongoDB connected...');
 };
 
 export default connectDB;
